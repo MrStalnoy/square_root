16fce86c41749639fd10814ddb222ea336de1a6e
MrStalnoy <44272702+MrStalnoy@users.noreply.github.com>
2023-02-16 17:36:59 +0300

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,8 @@
+import math
+
+def main():
+   number = float(input("Введите число: "))
+
+   square_root = math.sqrt(number)
+   print("Квадратный корень из ", number, 'Составляет ', square_root)
+main()
